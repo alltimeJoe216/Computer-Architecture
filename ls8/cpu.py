91f3afcb27fd5af0e6ee1ccee6a58cc8ba36d05e
Joe V <joeveverka89@gmail.com>
2020-09-16 12:43:26 -0400

    Day2 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,69 +1,86 @@
-"""CPU functionality."""
-
 import sys
+import os
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self, bits = 8):
         """Construct a new CPU."""
-        max_value = 2^bits
+        self.max_value = pow(2, bits)
 
         #State
         self.running = True
 
         # instructions...(pc), pc+1 and values (pc + 2)
-        self.ram = [0] * max_value
+        self.ram = ['0b0'] * self.max_value
 
         self.reg = [0] * bits
 
         #PC - Program counter = this will point to 1st byte of running instructions
         self.pc = 0
 
-        # address being read or written to
-        self.MAR = 0 
-        # value @ address 
-        self.MDR = 0 
-
         self.load()
 
         self.instructions = {
-            "LDI": 0b10000010,
-            "PRN": 0b01000111,
-            "HLT": 0b00000001
+            0b10000010: "LDI",
+            0b01000111: "PRN",
+            0b00000001: "HLT",
+            0b10100010: "MUL"
         }
-
-
 
     def load(self):
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        #filepath 
+        try:
+            filename = sys.argv[1]
+        except:
+            filename = 'mult.ls8'
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        cur_path = os.path.dirname(__file__)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        new_path = os.path.relpath(f'examples/{filename}', cur_path)
 
+        #load
+        with open(new_path) as f:
+            for line in f:
+                line = line.strip()
+                temp = line.split()
+
+                if len(temp) == 0:
+                    continue
+                if temp[0][0] == '#':
+                    continue
+
+                try:
+                    line = bin(int(temp[0], 2))
+                    self.ram[address] = line
+
+                except ValueError:
+                    print(f"Bad Num: {temp[0]}")
+                except IndexError:
+                    print(f"No addy at index {address}")
+                address += 1
+        for line in self.ram:
+            print(line)
+
+       
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.reg[reg_a] = (self.reg[reg_a] + self.reg[reg_b]) & 255
+        elif op == "MUL":
+            #255 is max_value mask
+            self.reg[reg_a] = (self.reg[reg_a] * self.reg[reg_b]) & 255
         else:
             raise Exception("Unsupported ALU operation")
+
+    def mul(self):
+        self.alu("MUL", self.ram_read(self.pc+1), self.ram_read(self.pc+2))
+
 
     def trace(self):
         """
@@ -87,13 +104,9 @@ class CPU:
 
     def ram_read(self, pc): 
         value = self.ram[pc]
-        self.MAR = pc
-        self.MDR = value
-        return value
+        return int(value, 2)
 
     def ram_write(self, pc, value):
-        self.MAR = pc
-        self.MDR = value
         self.ram[pc] = value
         return (pc, value)
 
@@ -108,15 +121,14 @@ class CPU:
         return self.ram_read(self.pc + 2)
 
     def ldi(self):
-        self.reg[self.current_reg()] = self.current_value() & 255
-        self.pc += 3
+        int_value = self.ram_read(self.pc + 2)
+        self.reg[self.pc] = int_value & 255
 
     def prn(self):
         print(self.reg[self.current_reg()])
 
-        self.pc += 2
 
-    def halt(self): 
+    def hlt(self): 
         self.running = False
 
     def run(self):
@@ -124,18 +136,24 @@ class CPU:
         while self.running:
             ir = self.ram_read(self.pc)
             #methods move ptr to next instruction
-            if ir == self.instructions["LDI"]:
-                self.ldi()
+            size = ir >> 6
+            shift = size + 1
 
-            elif ir == self.instructions["PRN"]:                
-                self.prn()
+            if ir in self.instructions:
 
-            elif ir == self.instructions["HLT"]:
-                self.halt()
+                instruction = getattr(CPU, self.instructions[self.ram_read(self.pc)].lower())
+                instruction(self)
+                print(instruction)
+                self.pc += shift 
+
+            else:
+                print("Bad instructions")
+                return
                 
 pc = CPU()
 pc.run()
 pc.trace()
+
 
 
 
