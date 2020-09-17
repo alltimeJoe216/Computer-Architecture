4290ed05a6a1f28e1ce2891b20e355472eb7aa7c
Joe V <joeveverka89@gmail.com>
2020-09-17 15:08:54 -0400

    implemented stack

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,15 +6,21 @@ class CPU:
 
     def __init__(self, bits = 8):
         """Construct a new CPU."""
-        self.max_value = pow(2, bits)
+        self.memory_size = pow(2, bits)
+        self.max_value = self.memory_size - 1
 
         #State
         self.running = True
 
-        # instructions...(pc), pc+1 and values (pc + 2)
-        self.ram = ['0b0'] * self.max_value
+        #Arch
+        self.ram = ['0b0'] * self.memory_size
 
-        self.reg = [0] * bits
+        self.num_registers = bits
+        self.reg = [0] * self.num_registers
+
+        #Stack
+        self.SP = self.num_registers - 1
+        self.reg[self.SP] = 0xf4 #F4 hex
 
         #PC - Program counter = this will point to 1st byte of running instructions
         self.pc = 0
@@ -25,7 +31,9 @@ class CPU:
             0b10000010: "LDI",
             0b01000111: "PRN",
             0b00000001: "HLT",
-            0b10100010: "MUL"
+            0b10100010: "MUL",
+            0b01000101: "PUSH",
+            0b01000110: "POP"
         }
 
     def load(self):
@@ -79,6 +87,8 @@ class CPU:
             raise Exception("Unsupported ALU operation")
 
     def mul(self):
+        print(self.reg[1])
+        print(f"multiplying {self.reg[self.ram_read(self.pc+1)]} and {self.reg[self.ram_read(self.pc+2)]}")
         self.alu("MUL", self.ram_read(self.pc+1), self.ram_read(self.pc+2))
 
 
@@ -122,7 +132,7 @@ class CPU:
 
     def ldi(self):
         int_value = self.ram_read(self.pc + 2)
-        self.reg[self.pc] = int_value & 255
+        self.reg[self.current_reg()] = int_value & 255
 
     def prn(self):
         print(self.reg[self.current_reg()])
@@ -144,7 +154,8 @@ class CPU:
                 instruction = getattr(CPU, self.instructions[self.ram_read(self.pc)].lower())
                 instruction(self)
                 print(instruction)
-                self.pc += shift 
+                self.pc += shift
+                print(f"shifted {shift}") 
 
             else:
                 print("Bad instructions")
