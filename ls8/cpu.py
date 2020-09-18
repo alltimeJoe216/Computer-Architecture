a7b903e546147c7e6c6ec09455ef78297f32ba8c
Joe V <joeveverka89@gmail.com>
2020-09-18 13:01:39 -0400

    MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,29 +6,38 @@ class CPU:
 
     def __init__(self, bits = 8):
         """Construct a new CPU."""
-        self.memory_size = pow(2, bits)
-        self.max_value = self.memory_size - 1
 
         #State
         self.running = True
 
-        #Arch
-        self.ram = [0] * self.memory_size
-
-        self.num_registers = bits
-        self.reg = [0] * self.num_registers
-
-        #Stack
-        self.SP = self.num_registers - 1
-        self.reg[self.SP] = 0xf4 #F4 hex
-
         #PC - Program counter = this will point to 1st byte of running instructions
         self.pc = 0
 
+        self.fl = 6
+        self.E = 0
+        self.L = 0
+        self.G = 0
+
+        # Machine Arch
+        self.memory_size = pow(2, bits)
+
+        # Don't max it out baby
+        self.max_value = self.memory_size - 1
+
+        #Our RAM holds instructions (pc), as well as registers (pc + 1), and values (pc + 2)
+        self.ram = [0] * self.memory_size
+
+        # The registers are the EXECUTORS of instructions
+        self.num_registers = bits
+        self.reg = [0] * self.num_registers
+
+        #Stack pointer
+        self.SP = self.num_registers - 1
+        self.reg[self.SP] = 0xf4 #F4 hex
+
+        # Instructions -> RAM
         self.load()
 
-        #Instructions -> RAM
-        
         self.instructions = {
             0b10000010: "LDI",
             0b01000111: "PRN",
@@ -46,14 +55,14 @@ class CPU:
         }
 
     def load(self):
-
+        # init
         address = 0
 
         #filepath 
         try:
             filename = sys.argv[1]
         except:
-            filename = 'call.ls8'
+            filename = 'sctest.ls8'
 
         cur_path = os.path.dirname(__file__)
 
@@ -72,7 +81,6 @@ class CPU:
 
                 try:
                     line = int(temp[0], 2)
-                    print(line)
                     self.ram_write(address, line)
 
                 except ValueError:
@@ -83,9 +91,8 @@ class CPU:
         f.close()
 
     def add(self):
-        self.alu("ADD", self.current_reg(), self.ram_read(self.pc+2))
-
-       
+        # add 2 reg values and write the result to first reg
+        self.alu("ADD", self.current_reg(), self.ram_read(self.pc+2))   
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -99,8 +106,8 @@ class CPU:
             raise Exception("Unsupported ALU operation")
 
     def mul(self):
+        # multiply 2 reg values and write to the first reg
         self.alu("MUL", self.ram_read(self.pc+1), self.ram_read(self.pc+2))
-
 
     def trace(self):
         """
@@ -108,10 +115,10 @@ class CPU:
         from run() if you need help debugging.
         """
 
+        print(self.pc)
+        print(self.ram[self.pc])
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.current_reg(),
             self.current_value()
@@ -130,9 +137,14 @@ class CPU:
         self.ram[pc] = value
         return (pc, value)
 
-    # def save_reg(self, reg, value):
-    #     self.reg[reg] = value
-    #     self.pc += 3 
+    def reg_read(self, pc):
+        #Read value from PC
+        value = self.reg[pc]
+        return value
+
+    def reg_write(self, pc, val):
+        self.reg[pc] = val
+        return (pc, val)
 
     def current_reg(self):
         return self.ram_read(self.pc + 1)
@@ -141,17 +153,51 @@ class CPU:
         return self.ram_read(self.pc + 2)
 
     def ldi(self):
+        # cop bit to the specified register (current_reg) if it fits
         int_value = self.current_value()
         self.reg[self.current_reg()] = int_value & self.max_value
 
     def prn(self):
+        # print programs current registry value
         print(self.reg[self.current_reg()])
 
+    def cmp2(self):
+        # compares 2 values. will return 1 if equal, 2 if greater, 4 if less
+
+        value1 = self.reg_read(self.current_reg())
+        value2 = self.reg_read(self.current_value())
+
+        if value1 == value2:
+            self.reg_write(self.fl, 1)
+        elif value1 > value2:
+            self.reg_write(self.fl, 2)
+        else:
+            self.reg_write(self.fl, 4)
+
+    def jmp(self):
+        # jump to register
+        self.pc = self.reg_read(self.current_reg())
+
+    def jeq(self):
+        # is value from the comparison equal
+        if self.reg_read(self.fl) == 1:
+            self.jmp()
+        # move pointer if not jumping    
+        else:
+            self.pc += 2
+
+    def jne(self):
+        # is value from comparison equal?
+        if self.reg_read(self.fl) != 1:
+            self.jmp()
+        else:
+            self.pc += 2
 
     def hlt(self): 
         self.running = False
 
-    def pop(self):        
+    def pop(self): 
+
         self.reg[self.current_reg()] = self.ram_read(self.reg[self.SP])
         self.reg[self.SP] += 1
 
@@ -160,20 +206,22 @@ class CPU:
         self.ram_write(self.reg[self.SP], self.reg[self.current_reg()])
 
     def call(self):
+        ## calling subroutine in the reg stack
         ret_addr = self.pc + 2
         self.reg[self.SP] -= 1
         self.ram_write(self.reg[self.SP], ret_addr)
         #call
         reg_num = self.ram_read(self.current_reg())
-        self.pc = self.reg[reg_num]
+        self.pc = self.reg_read(reg_num)
 
     def ret(self):
-        ret_addr = self.ram_read(self.reg[self.SP])
+        # set return value in reg stack
+        ret_addr = self.ram_read(self.reg_read(self.SP))
         self.reg[self.SP] += 1
         self.pc = ret_addr
 
-    def shift_bytes_by_ins_size(self):
-        ir = self.ram_read(self.pc)
+    def shift_bytes_by_ins_size(self, ir):
+        # shift ir to next instrution
         size = ir >> 6
         shift = size + 1
         self.pc += shift
@@ -184,13 +232,14 @@ class CPU:
             ir = self.ram_read(self.pc)
             
             if ir in self.instructions:
-
-                instruction = getattr(CPU, self.instructions[self.ram_read(self.pc)].lower())
+                # get the function associated with the value return from self.pc
+                ins_name = self.instructions[self.ram_read(self.pc)]
+                instruction = getattr(CPU, ins_name.lower())                  
+                # pass self in to the method since self.instruction() isn't defined (run the method)
                 instruction(self)
-                print(instruction)
-
+                # if not a call or ret, move instruction pointer
                 if ir & 0b0010000 == 0:
-                    self.shift_bytes_by_ins_size() 
+                    self.shift_bytes_by_ins_size(ir) 
 
             else:
                 print(f"Bad instructions: {ir}")
